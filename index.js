523fb98fbd5ea31073ba707f04fd24cd8678af68
insaneakx <insaneakx@gmail.com>
2020-10-02 16:50:21 +0300

    search endpoint added

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ app.get("/url", (req, res, next) => {
     res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
 });
 
+app.get('/search', function (req, res) {
+    setImmediate(function () {
+        try {
+            res.send('Success: ' + req.query.value)
+        } catch (e) {
+            res.status(400).send('Invalid JSON string')
+        }
+    })
+})
+
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
